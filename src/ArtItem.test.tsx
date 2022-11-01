27963d64166d2398c63b577707023ddb360d1f98
Meredith Latasa <meredith@lonsdaleinvest.com>
2022-10-31 21:40:55 -0400

    add tests

--- src/ArtItem.test.tsx
+++ src/ArtItem.test.tsx
@@ -2,11 +2,14 @@ import { cleanup, render, screen, fireEvent } from '@testing-library/react';
 import { ArtItem } from './ArtItem';
 import { artworks } from './constants';
 
-test('for an art item, submit button is disabled until a rating is selected', async () => {
-  const art = artworks[0];
+function getRandomIndex(n: number) {
+  return Math.floor(Math.random() * n) || 0;
+}
+
+test('for an art item, expect there to be no rating on load', async () => {
+  const i = getRandomIndex(artworks.length);
+  const art = artworks[i];
   render(<ArtItem artwork={art} artDetails={undefined} />);
-  const button = await screen.getByText('Submit');
-  expect(button).toBeDisabled();
 
   const ratings = screen.getAllByRole('radio');
   // first five radio buttons are stars
@@ -14,6 +17,20 @@ test('for an art item, submit button is disabled until a rating is selected', as
     expect(rating).not.toBeChecked();
   });
 
+  const ratingText = screen.getByTestId('rating');
+  expect(ratingText.innerHTML).toEqual('Unrated');
+
+  cleanup();
+});
+
+test('for an art item, submit button is disabled until a rating is selected', async () => {
+  const i = getRandomIndex(artworks.length);
+  const art = artworks[i];
+  render(<ArtItem artwork={art} artDetails={undefined} />);
+  const button = await screen.getByText('Submit');
+  expect(button).toBeDisabled();
+
+  const ratings = screen.getAllByRole('radio');
   const rating5 = ratings[4];
   fireEvent.click(rating5);
   expect(rating5).toBeChecked();
@@ -23,11 +40,13 @@ test('for an art item, submit button is disabled until a rating is selected', as
   });
 
   expect(button).not.toBeDisabled();
+
   cleanup();
 });
 
 test('for an art item, clicking star updates rating display below image to be that number', () => {
-  const art = artworks[0];
+  const i = getRandomIndex(artworks.length);
+  const art = artworks[i];
   render(<ArtItem artwork={art} artDetails={undefined} />);
 
   const ratingText = screen.getByTestId('rating');
@@ -38,14 +57,33 @@ test('for an art item, clicking star updates rating display below image to be th
   fireEvent.click(rating1);
   expect(ratingText.innerHTML).toEqual('1');
 
-  const rating5 = radioButtons[4];
-  fireEvent.click(rating5);
-  expect(ratingText.innerHTML).toEqual('5');
+  cleanup();
 });
 
-// test('for an art item, clicking numbered button updates rating display below image to be that number, clicking two different numbers one after the other', () => {});
+test('for an art item, clicking star updates rating display below image to be that number, clicking two different numbers one after the other updates it to the second number', () => {
+  const i = getRandomIndex(artworks.length);
+  const art = artworks[i];
+  render(<ArtItem artwork={art} artDetails={undefined} />);
 
-// test('for an art item, clicking submit POSTs update, displays a toast success message, hides buttons', () => {
-//   // The endpoint and payload for the submit button can be found in the submit method in `App.tsx`.
-//   // For the purpose of this test, please use a mock function instead.
-// });
+  const ratingText = screen.getByTestId('rating');
+  expect(ratingText.innerHTML).toEqual('Unrated');
+
+  const radioButtons = screen.getAllByRole('radio');
+  const rating1 = radioButtons[0];
+  const rating5 = radioButtons[4];
+  fireEvent.click(rating1);
+  fireEvent.click(rating5);
+  expect(ratingText.innerHTML).toEqual('5');
+
+  cleanup();
+});
+
+test('for an art item, clicking submit POSTs update, displays a toast success message, hides stars', () => {
+  // The endpoint and payload for the submit button can be found in the submit method in `App.tsx`.
+  // For the purpose of this test, please use a mock function instead.
+  const mockCallback = jest.fn(() => ({
+    message: 'Success',
+  }));
+
+  console.log('FUND', mockCallback);
+});

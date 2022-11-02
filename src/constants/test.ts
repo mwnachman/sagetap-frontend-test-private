e0effa0fbfd6c56c9923db8bc2e89e6d8efe634f
Meredith Latasa <meredith@lonsdaleinvest.com>
2022-11-02 17:27:05 -0400

    test helper func and mocks

---
+++ src/constants/test.ts
@@ -0,0 +1,52 @@
+import { FullArtDetails } from '../App';
+
+export const mockArtwork: FullArtDetails[] = [
+  {
+    data: {
+      id: 27999,
+      title: 'A Sunday on La Grande Jatte - 1884',
+      artist_title: 'Georges Seurat',
+      image_id: '8574f0a6-8713-bd6e-0f22-29af8894c36c',
+    },
+  },
+  {
+    data: {
+      id: 27999,
+      title: 'A Sunday on La Grande Jatte - 1884',
+      artist_title: 'Georges Seurat',
+      image_id: '8574f0a6-8713-bd6e-0f22-29af8894c36c',
+    },
+  },
+  {
+    data: {
+      id: 27999,
+      title: 'A Sunday on La Grande Jatte - 1884',
+      artist_title: 'Georges Seurat',
+      image_id: '8574f0a6-8713-bd6e-0f22-29af8894c36c',
+    },
+  },
+  {
+    data: {
+      id: 27999,
+      title: 'A Sunday on La Grande Jatte - 1884',
+      artist_title: 'Georges Seurat',
+      image_id: '8574f0a6-8713-bd6e-0f22-29af8894c36c',
+    },
+  },
+  {
+    data: {
+      id: 27999,
+      title: 'A Sunday on La Grande Jatte - 1884',
+      artist_title: 'Georges Seurat',
+      image_id: '8574f0a6-8713-bd6e-0f22-29af8894c36c',
+    },
+  },
+  {
+    data: {
+      id: 27999,
+      title: 'A Sunday on La Grande Jatte - 1884',
+      artist_title: 'Georges Seurat',
+      image_id: '8574f0a6-8713-bd6e-0f22-29af8894c36c',
+    },
+  },
+];
